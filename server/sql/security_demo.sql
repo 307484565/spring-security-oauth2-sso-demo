81744b1e3465f8e788be90bed8f2e5bf1a3958e8
zhangtao <tao.zhang@vhall.com>
2020-09-02 18:41:53 +0800

    update sql

--- server/sql/security_demo.sql
+++ server/sql/security_demo.sql
@@ -101,4 +101,23 @@ CREATE TABLE `user_role`  (
 INSERT INTO `user_role` VALUES (1, 1, 1);
 INSERT INTO `user_role` VALUES (2, 1, 2);
 
+
+CREATE TABLE oauth_client_details (
+    client_id VARCHAR(256) PRIMARY KEY,
+    resource_ids VARCHAR(256),
+    client_secret VARCHAR(256),
+    scope VARCHAR(256),
+    authorized_grant_types VARCHAR(256),
+    web_server_redirect_uri VARCHAR(256),
+    authorities VARCHAR(256),
+    access_token_validity INTEGER,
+    refresh_token_validity INTEGER,
+    additional_information VARCHAR(4096),
+    autoapprove VARCHAR(256)
+);
+
+INSERT INTO `oauth_client_details`(`client_id`, `resource_ids`, `client_secret`, `scope`, `authorized_grant_types`, `web_server_redirect_uri`, `authorities`, `access_token_validity`, `refresh_token_validity`, `additional_information`, `autoapprove`) VALUES ('client1', NULL, '$2a$10$H2trfldt4B3Oi9ZEolVob.uZB5SIjEPMr1yau8ydkwEHQr3RYggvO', 'user_info', 'authorization_code', 'http://127.0.0.1:8001/login', 'user:list', NULL, NULL, NULL, 'user_info');
+INSERT INTO `oauth_client_details`(`client_id`, `resource_ids`, `client_secret`, `scope`, `authorized_grant_types`, `web_server_redirect_uri`, `authorities`, `access_token_validity`, `refresh_token_validity`, `additional_information`, `autoapprove`) VALUES ('client2', NULL, '$2a$10$H2trfldt4B3Oi9ZEolVob.uZB5SIjEPMr1yau8ydkwEHQr3RYggvO', 'user_info', 'authorization_code', 'http://127.0.0.1:8002/login', NULL, NULL, NULL, NULL, 'user_info');
+
+
 SET FOREIGN_KEY_CHECKS = 1;
